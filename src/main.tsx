39ffccd7f7561b255530b70606c1a8c3791aca69
Alesya <alesyatix@gmail.com>
2026-06-23 00:20:58 +0300

    fix github pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <BrowserRouter basename="/TheInterior04.26">
+    <BrowserRouter basename="/TheInterior04.26/">
       <App />
     </BrowserRouter>
   </HelmetProvider>
